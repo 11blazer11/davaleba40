e3c400a15f988ded662ea261c9bf458ce8f20230
unknown <bebiashvili.dachi@gmail.com>
2026-03-22 11:07:10 +0400

    dsf

--- products/models.py
+++ products/models.py
@@ -39,32 +39,4 @@ class Product(BaseModel):
     def average_rating(self):
         pass
 
-
-class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True, blank=True)
-    content = models.TextField()
-    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
-
-
-class FavoriteProduct(models.Model):
-    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True, blank=True)
-
-
-class ProductTag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Cart(models.Model):
-    products = models.ManyToManyField('products.Product', related_name='carts')
-    user = models.OneToOneField('users.User', on_delete=models.SET_NULL, null=True, blank=True)
-
-
-class ProductImage(models.Model):
-    image = models.ImageField(upload_to='products/')
-    product = models.ForeignKey('products.Product', related_name='images', on_delete=models.CASCADE)
     
--- products/urls.py
+++ products/urls.py
@@ -1,45 +1,12 @@
-from django.urls import path
-from . import views
+# from .views import 
 from rest_framework.routers import SimpleRouter, DefaultRouter
 
 urlpatterns = [
-    # path("products/", views.products, name="products"),
-    path("reviews/", views.review_view, name="review_view"),
-
-    path('product-apiview/', views.ProductView.as_view(), name='product'),
-    path('product-apiview/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-
-    path('products-genericapiview/', views.ProductAPIView.as_view(), name="products_genericapiview"),
-    path('products-genericapiview/<int:pk>', views.ProductAPIView.as_view(), name='product_genericapiview'),
-
-    path("favorite-product/", views.FavoriteProductViewSet.as_view(), name="favorite_product"),
-    path("favorite-product/<int:pk>/", views.FavoriteProductViewSet.as_view()),
-
-    path("cart-flow/", views.CartView.as_view()),
-
-    path("tag-list/", views.TagView.as_view()),
-    path("tags/", views.TagList.as_view()),
-
-    path("review-view/<int:product_id>/", views.ReviewView.as_view()),
-
-    path('product-model-view-set/', views.ProductModelViewSet.as_view({"get": "list", "post": "create"})),
-    path('product-model-view-set/<int:pk>/', views.ProductModelViewSet.as_view({"get": "retrieve", "put": "update", "patch": "partial_update", "delete": "destroy"})),
 
 ]
 
-from rest_framework_nested import routers
-
-# router = SimpleRouter()
 router = DefaultRouter()
-router.register('products', views.ProductModelViewSet)
+router.register()
 
-product_router = routers.NestedDefaultRouter(
-    router,
-    'products',
-    lookup='product',
-)
-
-product_router.register('images', views.ProductImageViewSet, basename="product_image")
 
 urlpatterns += router.urls
-urlpatterns += product_router.urls
